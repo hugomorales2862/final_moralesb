b1d31534b0fa233d9d7cd2232afd5cd9377a55a8
Hugo Morales <moralesbatzh@gmail.com>
2023-06-10 14:00:37 -0600

    creamos nuestra clase Grado con guardar, buscar, modifcar y eliminar

---
+++ modelos/Grado.php.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'Conexion.php';
+class Grado extends Conexion{
+    public $ID;
+    public $gra_descripcion;
+    public $gra_situacion;
+
+    public function __construct($args = [] )
+    {
+        $this-> ID = $args ['ID'] ?? NULL;
+        $this-> gra_descripcion = $args  ['gra_descripcion'] ?? ' '; 
+    }
+
+   public function guardar (){
+    $sql = "INSERT INTO grados (gra_descripcion)values ('$this->gra_descripcion')";
+    $resultado =  self::ejecutar($sql);
+    return $resultado;
+   }
+
+
+    public function buscar(){
+        $sql =  " SELECT * FROM grados where gra_situacion = 1";
+        if($this->gra_descripcion != ' '){
+            $sql .= " and gra_descripcion like '%$this->gra_descripcion%'";
+            }
+        $resultado = self::servir($sql);
+        return $resultado;
+
+    }
+
+    public function modificar (){
+        $sql = "UPDATE grados SET gra_descripcion = '$this->gra_descripcion' where ID = $this->ID ";
+        $resultado = self::ejecutar($sql);
+        return $resultado;    
+    }
+
+    public function eliminar(){
+        $sql = "UPDATE grados SET gra_situacion = 0 where ID = $this->ID";
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+
+}
+?>
