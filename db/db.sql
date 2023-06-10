58565dfb6c05b613ca92aac67060daf1b640a43c
Hugo Morales <moralesbatzh@gmail.com>
2023-06-10 17:11:00 -0600

    se realizaron cambios en la base de datos

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,29 @@
+-- Creación de la tabla "Materias"
+CREATE TABLE materias (
+  mat_id SERIAL PRIMARY KEY,
+  mat_nombre VARCHAR(50) NOT NULL,
+  mat_situacion CHAR(1) DEFAULT '1'
+);
+
+-- tabla de nacionalidades 
+CREATE TABLE nacionalidades(
+    nac_id SERIAL PRIMARY KEY,
+    nac_pais VARCHAR (50) NOT NULL,
+    nac_situacion CHAR(1) DEFAULT '1'
+    )
+-- tabla de grados 
+CREATE TABLE grados(
+    gra_id SERIAL PRIMARY KEY,
+    gra_descripcion VARCHAR (50) NOT NULL,
+    gra_situacion CHAR(1) DEFAULT '1'
+)
+
+-- tabla de armas
+CREATE TABLE armas (
+    arm_id SERIAL PRIMARY KEY,
+    arm_descripcion VARCHAR (50) NOT NULL,
+    arm_situacion CHAR(1) DEFAULT '1'
+); 
 -- Creación de la tabla "Alumnos"
 CREATE TABLE alumnos (
   alu_id  SERIAL PRIMARY KEY,
@@ -7,49 +33,28 @@ CREATE TABLE alumnos (
   alu_arma INT NOT NULL,
   alu_nac INT NOT NULL,
   alu_situacion CHAR(1) DEFAULT '1',
-  FOREIGN KEY (alu_grado) REFERENCES grados(ID),
-  FOREIGN KEY (alu_arma) REFERENCES armas(ID),
-   FOREIGN KEY (alu_nac) REFERENCES nacionalidades(ID));
+  FOREIGN KEY (alu_grado) REFERENCES grados(gra_id),
+  FOREIGN KEY (alu_arma) REFERENCES armas(arm_id),
+   FOREIGN KEY (alu_nac) REFERENCES nacionalidades(nac_id));
 
--- Creación de la tabla "Materias"
-CREATE TABLE materias (
-  mat_id SERIAL PRIMARY KEY,
-  mat_nombre VARCHAR(50) NOT NULL,
-  mat_situacion CHAR(1) DEFAULT '1';
-);
-
--- tabla de nacionalidades 
-CREATE TABLE nacionalidades(
-    mat_id SERIAL PRIMARY KEY,
-    nac_pais VARCHAR (50) NOT NULL,
-    nac_situacion CHAR(1) DEFAULT '1';
-    )
--- tabla de grados 
-CREATE TABLE grados(
-    mat_id SERIAL PRIMARY KEY,
-    gra_descripcion VARCHAR (50) NOT NULL,
-    gra_situacion CHAR(1) DEFAULT '1';
-)
-
--- tabla de armas
-CREATE TABLE armas (
-    arma_id SERIAL PRIMARY KEY,
-    arm_descripcion VARCHAR (50) NOT NULL,
-    arm_situacion CHAR(1) DEFAULT '1';
-); 
 
 -- Creación de la tabla "Calificaciones"
 CREATE TABLE asig_materia (
   asig_alumno INT,
   asig_materia INT,
-  PRIMARY KEY (asig_alumno,asig_materia)
-  FOREIGN KEY (alumno_id) REFERENCES alumnos(id),
-  FOREIGN KEY (materia_id) REFERENCES materias(id)
+  asig_situacion CHAR(1) DEFAULT '1',
+  PRIMARY KEY (asig_alumno,asig_materia),
+  FOREIGN KEY (asig_alumno) REFERENCES alumnos(alu_id),
+  FOREIGN KEY (asig_materia) REFERENCES materias(mat_id)
 );
 
 CREATE TABLE calificaciones (
   calif_id SERIAL PRIMARY KEY,
-  calif_descripcion INT,
-  calificacion INT,
-  FOREIGN KEY (calif_descripcion) REFERENCES asig_materia(asig_alumno, asig_materia)
+  calif_alumno INT,
+  calif_materia INT,
+  calif_nota FLOAT NOT NULL,
+  calif_resultado VARCHAR(50) NOT NULL,
+  calif_situacion CHAR(1) DEFAULT '1',
+  FOREIGN KEY (calif_alumno, calif_materia) REFERENCES asig_materia(asig_alumno, asig_materia)
 );
+
