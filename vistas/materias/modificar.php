1736dcdc65b63b8b83d5bc7005ca39e78db90e59
Hugo Morales <moralesbatzh@gmail.com>
2023-06-11 00:13:41 -0600

    se corrigio el archivo modificar para la clase materia

--- vistas/materias/modificar.php
+++ vistas/materias/modificar.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require __DIR__.'/../../modelos/Grado.php';
+require __DIR__.'/../../modelos/Materia.php';
     try {
         $grado = new Grado($_GET);
 
