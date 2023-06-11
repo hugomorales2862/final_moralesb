f9da4464eda0b953c4e84b4ba8a744b4b3f02304
Hugo Morales <moralesbatzh@gmail.com>
2023-06-11 00:53:27 -0600

    se creo la vista buscar para la clase Nacionalidad

--- vistas/nacionalidades/buscar.php
+++ vistas/nacionalidades/buscar.php
@@ -1,13 +1,13 @@
 <?php include_once __DIR__.'/../../includes/header.php' ?>
 <?php include_once __DIR__. '/../../includes/navbar.php' ?>
     <div class="container">
-        <h1 class="text-center">Buscar Materias del curso</h1>
+        <h1 class="text-center">Buscar Nacionalidad del alumno</h1>
         <div class="row justify-content-center">
-            <form action="../../controladores/materias/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
+            <form action="../../controladores/nacionalidades/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="mat_nombre">Materias</label>
-                        <input type="text" name="mat_nombre" id="mat_nombre" class="form-control" required>
+                        <label for="nac_pais">Pais</label>
+                        <input type="text" name="nac_pais" id="nac_pais" class="form-control" required>
                     </div>
                 </div>
         
