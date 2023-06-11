7749eeaeb59b8b03d5d77812c83a489b6fcff44b
Hugo Morales <moralesbatzh@gmail.com>
2023-06-10 20:40:41 -0600

    se modifico el navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,53 +4,83 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="#">Tienda virtual</a>
+            <a class="navbar-brand" href="#">Control de Notas</a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Ventas
+                        GRADO
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/crud_practica9/vistas/ventas/index.php">Ingresar venta</a>
+                            <a class="nav-link" href="../vistas/grados/index.php">Grados Militares</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/crud_practica9/vistas/ventas/buscar.php">Buscar venta</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/crud_practica9/controladores/ventas/detalle_ventas.php">Detalle de venta</a>
+                            <a class="nav-link" href="../vistas/grados/buscar.php">Buscar Grado</a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Productos
+                        NACIONALIDAD
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/crud_practica9/vistas/productos/index.php">Crear producto</a>
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/index.php">Registrar pais</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/crud_practica9/vistas/productos/buscar.php">Buscar producto</a>
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/buscar.php">Buscar pais</a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Clientes
+                        MATERIAS
                     </a>
                     <ul class="dropdown-menu bg-dark">
                         <li class="nav-item">
-                            <a class="nav-link" href="/crud_practica9/vistas/clientes/index.php">Crear cliente</a>
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/index.php">Registrar materias</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/crud_practica9/vistas/clientes/buscar.php">Buscar cliente</a>
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/buscar.php">Asignar alumno</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/buscar.php">Buscar materia</a>
                         </li>
                     </ul>
                 </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        ARMAS
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/index.php">Registro Arma/servicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/buscar.php">Buscar Arma/servicio</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        ALUMNOS
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/index.php">Registrar alumnos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/buscar.php">Notas del alumno</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/buscar.php">Buscar Alumno</a>
+                        </li>
+                    </ul>
+                </li>
+
             </ul>
         </div>
     </div>
