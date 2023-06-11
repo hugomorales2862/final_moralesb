3e077ab4750887525e91871d5767024fb52567af
Hugo Morales <moralesbatzh@gmail.com>
2023-06-10 22:27:49 -0600

    modificacion del archivo buscar para la clase arma

--- vistas/grados/buscar.php
+++ vistas/grados/buscar.php
@@ -1,13 +1,13 @@
 <?php include_once __DIR__.'/../../includes/header.php' ?>
 <?php include_once __DIR__. '/../../includes/navbar.php' ?>
     <div class="container">
-        <h1 class="text-center">Arma o Servicio</h1>
+        <h1 class="text-center">Buscar Grados Militares</h1>
         <div class="row justify-content-center">
-            <form action="../../controladores/armas/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
+            <form action="../../controladores/grados/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="arm_descripcion">Arma/Servicio</label>
-                        <input type="text" name="arm_descripcion" id="arm_descripcion" class="form-control" required>
+                        <label for="gra_descripcion">Grado Militar</label>
+                        <input type="text" name="gra_descripcion" id="gra_descripcion" class="form-control" required>
                     </div>
                 </div>
         
