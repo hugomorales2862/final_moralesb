98a556c9bac24559dca1deeac4d7fdedeaa8c225
Hugo Morales <moralesbatzh@gmail.com>
2023-06-11 00:00:34 -0600

    se creo el archivo index para la clase materia

--- vistas/materias/buscar.php
+++ vistas/materias/buscar.php
@@ -1,13 +1,13 @@
 <?php include_once __DIR__.'/../../includes/header.php' ?>
 <?php include_once __DIR__. '/../../includes/navbar.php' ?>
     <div class="container">
-        <h1 class="text-center">Buscar Grados Militares</h1>
+        <h1 class="text-center">Buscar Materias del curso</h1>
         <div class="row justify-content-center">
-            <form action="../../controladores/grados/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
+            <form action="../../controladores/materias/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="gra_descripcion">Grado Militar</label>
-                        <input type="text" name="gra_descripcion" id="gra_descripcion" class="form-control" required>
+                        <label for="mat_nombre">Materias</label>
+                        <input type="text" name="mat_nombre" id="mat_nombre" class="form-control" required>
                     </div>
                 </div>
         
