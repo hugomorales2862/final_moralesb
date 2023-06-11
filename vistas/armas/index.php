5805e131dce8c720815bb46d7a0f2cc048ee9a34
Hugo Morales <moralesbatzh@gmail.com>
2023-06-10 22:30:34 -0600

    se modifico el archivo index para la clase arma

--- vistas/armas/index.php
+++ vistas/armas/index.php
@@ -6,12 +6,12 @@ error_reporting(E_ALL);
 <?php include_once __DIR__.'/../../includes/header.php' ?>
 <?php include_once __DIR__. '/../../includes/navbar.php' ?>
 <div class="container">
-    <h1 class="text-center">Formulario de ingreso de Grados militares </h1>
+    <h1 class="text-center">Formulario de Ingreso de Armas o Servicios </h1>
     <div class="row justify-content-center">
-        <form action="../../controladores/grados/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+        <form action="../../controladores/armas/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
             <div class="row mb-3">
                 <div class="col">
-                    <label for="gra_descripcion">Grado militar</label>
+                    <label for="gra_descripcion">Arma/Servicio</label>
                     <input type="text" name="gra_descripcion" id="gra_descripcion" class="form-control">
                 </div>
             </div>
